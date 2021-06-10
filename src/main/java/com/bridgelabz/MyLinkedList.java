f53299198445f58ce3f5a9ea70b7dce795310556
mohammedsalmannn <rmdsalman001@gmail.com>
2021-06-10 08:13:21 +0530

    Added : Function to Add Node at the Top

--- src/main/java/com/bridgelabz/MyLinkedList.java
+++ src/main/java/com/bridgelabz/MyLinkedList.java
@@ -20,4 +20,28 @@ public class MyLinkedList<K> {
             this.tail = myNode;
         }
     }
+
+    public void add(INode newNode ) {
+        if (this.tail == null)
+            this.tail = newNode ;
+        if (this.head == null)
+            this.head = newNode ;
+        else {
+            INode tempNode = this.head;
+            this.head = newNode;
+            this.head.setNext(tempNode);
+        }
+    }
+
+    public void printMyNodes() {
+        StringBuffer myNodes = new StringBuffer("My Nodes: ");
+        INode tempNode = head;
+        while (tempNode.getNext() != null) {
+            myNodes.append(tempNode.getKey());
+            if (!tempNode.equals(tail)) myNodes.append("->");
+            tempNode = tempNode.getNext();
+        }
+        myNodes.append(tempNode.getKey());
+        System.out.println(myNodes);
+    }
 }
